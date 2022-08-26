b7d48b65352694a0614533bb10e401cdefccc55b
shotpoet86 <curlybracemaster@gmail.com>
2022-08-25 23:42:57 -0400

    added MainNav component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1>Hello world</h1>
+  <MainNav nav="Main Nav"></MainNav>
+  <MainNav nav="Main Nav"></MainNav>
+  <main-nav nav="Main K"></main-nav>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-full text-sm bg-brand-blue-1">{{ nav }}</header>
+  <div class="fixed top-0 left-0 w-full h-16 bg-white">
+    <div
+      class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
+    >
+      <a href="/" class="flex items-center h-full text-xl">Boogle Careers</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  props: {
+    nav: String,
+  },
+};
+</script>
